687e266e477ed797572ed5e3294f8bc7ac55b19e
bhathiya <bhathiyabandara87@gmail.com>
2022-03-28 00:17:33 +0530

    completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState,useEffect }from "react";
+import Axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [listOfUsers,setListOfUsers]=useState([]);
+  const[name, setName]=useState("");
+  const[age, setAge]=useState("0");
+  const[username, setuserName]=useState("");
+
+  useEffect(() =>{
+      Axios.get("http://localhost3001/getUsers").then((response)=>{
+        setListOfUsers(response.data)
+      });
+  }, []);
+
+  const createUser =()=>{
+      Axios.post("http://localhost3001/createUser",{
+        name,
+        age,
+        username,
+      }).then((response)=>{
+        setListOfUsers([...listOfUsers,{name:name,age:age,username:username}]);
+      });
+  };
+
+  return <div class="App">
+    <div class="usersDisplay">
+
+    {listOfUsers.map((user) => {
+      return(<div>
+      
+      <h1>Name: {user.name}</h1>
+      <h1>Age: {user.age}</h1>
+      <h1>UserName: {user.username}</h1>
+      
+      </div>
+      );
+    })};
+
     </div>
-  );
+
+    <div>
+      <input type="text" placeholder="Name" onchange={(event)=>{setName(event.target.value);}}></input>
+      <input type="numver" placeholder="Age"onchange={(event)=>{setAge(event.target.value);}}></input>
+      <input type="text" placeholder="UserName"onchange={(event)=>{setuserName(event.target.value);}}></input>
+      <button onclick={createUser}>Create User</button>
+    </div>
+  </div>
+
 }
+
 
 export default App;
